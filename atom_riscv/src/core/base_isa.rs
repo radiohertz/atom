c15c6d6e00312587c3f5ea7abb68251963699938
David John <33180269+int0xh@users.noreply.github.com>
2021-08-02 20:57:33 +0530

    proj: fix indentation

--- atom_riscv/src/core/base_isa.rs
+++ atom_riscv/src/core/base_isa.rs
@@ -1,31 +1,35 @@
 use super::{hart::{Hart, RWreg}};
 
 
+pub enum Rv32iInst {
+    ADDI(u8, u8,u16),
+    ADD(u8,u8,u8)
+}
 
 
 /// The base RISCV ISA.
 pub trait Rv32i {
 
-	/// The `addi` instruction of type `I`.
-	pub fn addi(&mut self, rd: u8, rs1: u8, imm: u16);
+    /// The `addi` instruction of type `I`.
+    fn addi(&mut self, rd: u8, rs1: u8, imm: u16);
 
-	/// The `add` instruction of type `R`.
-	pub fn add(&mut self, rd: u8, rs1: u8, rs2: u8);
+    /// The `add` instruction of type `R`.
+    fn add(&mut self, rd: u8, rs1: u8, rs2: u8);
 }
 
 
 impl Rv32i for Hart {
 
-	fn addi(&mut self, rd: u8, rs1: u8, imm: u16) {
-		let r_val = self.read_reg(rs1);
-		self.write_reg(rd, r_val + imm as u64);
-	}
+    fn addi(&mut self, rd: u8, rs1: u8, imm: u16) {
+        let r_val = self.read_reg(rs1);
+        self.write_reg(rd, r_val + imm as u64);
+    }
 
-	/// The `add` instruction of type `R`.
-	fn add(&mut self, rd: u8, rs1: u8, rs2: u8) {
-		let v1 = self.read_reg(rs1);
-		let v2 = self.read_reg(rs2);
-		self.write_reg(rd, v1 + v2);
-	}
+    /// The `add` instruction of type `R`.
+    fn add(&mut self, rd: u8, rs1: u8, rs2: u8) {
+        let v1 = self.read_reg(rs1);
+        let v2 = self.read_reg(rs2);
+        self.write_reg(rd, v1 + v2);
+    }
 }
 
--- atom_riscv/src/core/hart.rs
+++ atom_riscv/src/core/hart.rs
@@ -3,140 +3,148 @@ pub use super::base_isa::Rv32i;
 /// A RISC-V Hardware thread.
 /// Contains its own registers and program counter.
 pub struct Hart {
-	/// 32 General purpose registers.
-	x: [u64;32],
-	/// 64 bit program counter.
-	pc: u64,
-	/// The application code that hart is going to execute.
-	code: Option<Vec<u8>>
+    /// 32 General purpose registers.
+    x: [u64;32],
+    /// 64 bit program counter.
+    pc: u64,
+    /// The application code that hart is going to execute.
+    code: Option<Vec<u8>>
 }
 
 
 impl Hart {
-	/// Create a new hart (hardware thread).
-	/// `code` - the code the thread is going to execute.
-	pub fn new(code: Option<Vec<u8>>) -> Self {
-		Hart{
-			x: [0;32],
-			pc: 0x0,
-			code: code
-		}
-	}
+    /// Create a new hart (hardware thread).
+    /// `code` - the code the thread is going to execute.
+    pub fn new(code: Option<Vec<u8>>) -> Self {
+        Hart{
+            x: [0;32],
+            pc: 0x0,
+            code: code
+        }
+    }
 
-	/// Fetch the next instruction.
-	fn fetch(&self) -> u32 {
-		let idx = self.pc as usize;
-		assert!(idx % 4 == 0, "Unaligned instruction memory access");
+    /// Fetch the next instruction.
+    fn fetch(&self) -> u32 {
+        let idx = self.pc as usize;
+        assert!(idx % 4 == 0, "Unaligned instruction memory access");
 
-		let code = self.code.as_ref().unwrap();
+        let code = self.code.as_ref().unwrap();
 
-		(code[idx] as u32) |
-		(code[idx + 1] as u32) << 8 |
-		(code[idx + 2] as u32) << 16 |
-		(code[idx + 3] as u32) << 24
-	}
+        (code[idx] as u32) |
+        (code[idx + 1] as u32) << 8 |
+        (code[idx + 2] as u32) << 16 |
+        (code[idx + 3] as u32) << 24
+    }
 
-	fn decode(&mut self, inst: u32) {
-		match inst {
-			0x13 => {
-				self.addi();
+    fn decode(&mut self, inst: u32) {
+        let opcode = inst & 0x3f;
 
-			},
-			0x33 => {
-				self.add();
-			}
-			_ => {
+        match opcode {
+            0x13 => {
+                println!("ADDI")
+            },
+            0x33 => {
+                println!("ADD")
+            }
+            _ => {
 
-			}
-		}
+            }
+        }
 
-	}
+    }
 
-	fn execute(&mut self) { 
-	}
+    fn execute(&mut self) { 
+    }
 
-	pub fn run(&mut self) {
-		let inst = self.fetch();
-		self.pc += 4;
-		self.decode(inst);
+    pub fn run(&mut self) {
+        let len = self.code.as_ref().unwrap().len();
 
-	}
+        loop {
+            if self.pc >= len as u64 {
+                break;
+            }
+            let inst = self.fetch();
+            self.pc += 4;
+            self.decode(inst);
+        }
 
-	/// Read and write to the registers using the `RWreg`
-	fn reg_rw(&mut self, op: RWreg) -> Option<u64> {
-		match op {
-			RWreg::Read(i) => Some(*self.x.get(i as usize).unwrap()),
-			RWreg::Write(reg,val) => {
-				*self.x.get_mut(reg as usize).unwrap() = val;
-				None
-			}
-		}
-	}
+    }
+
+    /// Read and write to the registers using the `RWreg`
+    fn reg_rw(&mut self, op: RWreg) -> Option<u64> {
+        match op {
+            RWreg::Read(i) => Some(*self.x.get(i as usize).unwrap()),
+            RWreg::Write(reg,val) => {
+                *self.x.get_mut(reg as usize).unwrap() = val;
+                None
+            }
+        }
+    }
 
 
-	pub fn read_reg(&mut self, reg: u8) -> u64 {
-		self.reg_rw(RWreg::Read(reg)).unwrap()
-	}
+    pub fn read_reg(&mut self, reg: u8) -> u64 {
+        self.reg_rw(RWreg::Read(reg)).unwrap()
+    }
 
-	pub fn write_reg(&mut self, reg: u8, val: u64) {
-		assert_eq!(self.reg_rw(RWreg::Write(reg, val)), None);
-	}
+    pub fn write_reg(&mut self, reg: u8, val: u64) {
+        assert_eq!(self.reg_rw(RWreg::Write(reg, val)), None);
+    }
 
-	pub fn debug(&self) {
-		for (i,v) in self.x.iter().enumerate() {
-			println!("{} - {}", i, v);
-		}
-	}
+    pub fn debug(&self) {
+        for (i,v) in self.x.iter().enumerate() {
+            println!("{} - {}", i, v);
+        }
+    }
 }
 
 pub enum RWreg {
-	Read(u8),
-	Write(u8,u64)
+    Read(u8),
+    Write(u8,u64)
 }
 
 
 /// Various instruction encoding types used by RISC-V.
 #[derive(Debug,PartialEq)]
 pub enum InstType {
-	R,
-	I,
-	S,
-	U
+    R,
+    I,
+    S,
+    U
 }
 
 
 impl InstType {
-	fn which(inst: u32) -> Self {
-		// isolate the opcode.
-		let opcode = inst & 0x3f;
-		match opcode {
-			0x13 => InstType::I,
-			0x33 => InstType::R,
-			_ => panic!("Should'nt come here")
-		}
-	}
+    fn which(inst: u32) -> Self {
+        // isolate the opcode.
+        let opcode = inst & 0x3f;
+        match opcode {
+            0x13 => InstType::I,
+            0x33 => InstType::R,
+            _ => panic!("Should'nt come here")
+        }
+    }
 }
 
 
 #[derive(Debug)]
 pub struct IType {
-	pub(crate) opcode: u32,
-	pub(crate) rd: u8,
-	pub(crate) funct3: u8,
-	pub(crate) rs1: u8,
-	pub(crate) imm: u16
+    pub(crate) opcode: u32,
+    pub(crate) rd: u8,
+    pub(crate) funct3: u8,
+    pub(crate) rs1: u8,
+    pub(crate) imm: u16
 }
 
 #[test]
 fn test_correct_decoded_instruction_type() {
-	assert_eq!(InstType::which(0x13), InstType::I);
-	assert_eq!(InstType::which(0x33), InstType::R);
+    assert_eq!(InstType::which(0x13), InstType::I);
+    assert_eq!(InstType::which(0x33), InstType::R);
 }
 
 #[test]
 #[should_panic]
 fn test_register_write_to_none() {
-	let mut hart = Hart::new(None);
-	hart.reg_rw(RWreg::Write(32,0x1337));
+    let mut hart = Hart::new(None);
+    hart.reg_rw(RWreg::Write(32,0x1337));
 }
 
